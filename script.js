25856a5198a66df68c407ec5c4ef3b8867161415
Sourabh Singh <the.sourabh045@gmail.com>
2023-07-25 13:50:49 +0530

    certi

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const container1 = document.querySelector('.container1')
 open1.addEventListener('click', () => container1.classList.add('show-nav'))
 
 close1.addEventListener('click', () => container1.classList.remove('show-nav'))
-window.onscroll=function(){myfunction()};
-function myfunction(){
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10){
+window.onscroll = function () { myfunction() };
+function myfunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         container1.classList.remove('show-nav');
-    }   
+    }
 };
 // day mode night mode button
-const night=document.getElementById('butu')
-night.addEventListener('click',() => container1.classList.toggle('darkmode'));
+const night = document.getElementById('butu')
+night.addEventListener('click', () => container1.classList.toggle('darkmode'));
 
 var typed = new Typed('#element', {
     strings: ['Web Developer &', 'Python Programmer'],
